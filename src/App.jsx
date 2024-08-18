239bd67728725fe8f964e2b0f82d96a2913f4ce2
Mohammed Salem <m.almuntheri@you-it.com>
2024-08-18 15:36:18 +0300

    edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
-import Footer from './components/Footer'
 import BookingForm from './components/BookingForm'
 import Categories from './components/Categories'
 import Meals from './components/Meals'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
@@ -20,7 +17,6 @@ function App() {
 	  </div>
 	  <Meals />
 	  <BookingForm />
-	  <Footer />
     </>
   )
 }
